c1a9582625e876a59a94b67be95670fcc4f91992
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-06 11:56:07 -0300

    Adicionando método PUT - editar produto

--- Farmacia/src/main/java/com/generation/farmacia/controller/ProdutoController.java
+++ Farmacia/src/main/java/com/generation/farmacia/controller/ProdutoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,14 @@ public class ProdutoController {
 	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(produtoRepository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
+		return produtoRepository.findById(produto.getId())
+				.map(resposta ->ResponseEntity.status(HttpStatus.OK)
+					.body(produtoRepository.save(produto)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
 }
