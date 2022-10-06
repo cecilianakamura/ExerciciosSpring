e68c5925bad42b231c0958e73290d25775b20c6b
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-06 12:09:15 -0300

    Criando Classes Model e Repository para Categorias

---
+++ Farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,38 @@
+package com.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message="O nome da categoria é obrigatório")
+	private String categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
