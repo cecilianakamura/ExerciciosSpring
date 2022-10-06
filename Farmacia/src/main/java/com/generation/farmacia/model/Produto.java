5741e59e58a8f42d8f821894ecfa2a59e581ce19
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-06 12:12:23 -0300

    Criando relação ManyToOne na Classe Produto

--- Farmacia/src/main/java/com/generation/farmacia/model/Produto.java
+++ Farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -13,6 +14,7 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_produtos")
@@ -39,6 +41,10 @@ public class Produto {
 	@Positive(message="O preço deve ser maior que zero.")
 	private BigDecimal preco;
 
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
 	public Long getId() {
 		return id;
 	}
