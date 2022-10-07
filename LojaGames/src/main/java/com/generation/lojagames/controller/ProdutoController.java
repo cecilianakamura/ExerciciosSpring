896e5fdf052649b79b4529092685b241db7b3a88
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-07 14:09:53 -0300

    Criando método getAll

--- LojaGames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
+++ LojaGames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
@@ -1,10 +1,15 @@
 package com.generation.lojagames.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.lojagames.model.Produto;
 import com.generation.lojagames.repository.ProdutoRepository;
 
 @RestController
@@ -14,4 +19,11 @@ public class ProdutoController {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){
+		return ResponseEntity.ok(produtoRepository.findAll());
+		
+		
+	}
 }
