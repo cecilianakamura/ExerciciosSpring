bd305474b4a2cd4a6d1d021e1e5b263ca01eba50
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-07 19:40:40 -0300

    Criando relação OneToMany na classe Categoria

--- LojaGames/src/main/java/com/generation/lojagames/model/Categoria.java
+++ LojaGames/src/main/java/com/generation/lojagames/model/Categoria.java
@@ -1,12 +1,18 @@
 package com.generation.lojagames.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_categorias")
@@ -19,6 +25,19 @@ public class Categoria {
 	@NotBlank
 	@Size(min=3,max=255,message="O nome da categoria é obrigatório.")
 	private String nome;
+
+	@OneToMany(mappedBy="categoria",cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
+	
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 
 	public Long getId() {
 		return id;
