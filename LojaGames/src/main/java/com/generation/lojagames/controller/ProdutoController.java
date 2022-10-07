43352da53b3bd32504d8f4a5067b5325ee1025c0
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-07 14:45:47 -0300

    Adicionando métodos PUT e POST

--- LojaGames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
+++ LojaGames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
@@ -2,12 +2,17 @@ package com.generation.lojagames.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,4 +48,20 @@ public class ProdutoController {
 		return ResponseEntity.ok(produtoRepository.findByDescricaoContainingIgnoreCase(descricao));
 	}
 
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(produtoRepository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
+		return produtoRepository.findById(produto.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+					.body(produtoRepository.save(produto)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	
+	
 }
