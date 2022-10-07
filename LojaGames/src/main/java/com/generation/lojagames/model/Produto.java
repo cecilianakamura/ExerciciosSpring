de5ae3e6ec12b90762923a36bdc16f1f47744ece
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-07 12:23:22 -0300

    Criando uma model para Produto

--- LojaGames/src/main/java/com/generation/lojagames/model/Produto.java
+++ LojaGames/src/main/java/com/generation/lojagames/model/Produto.java
@@ -1,5 +1,27 @@
 package com.generation.lojagames.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_produtos")
 public class Produto {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="O nome do produto é obrigatório.")
+	@Size(min=3,max=255,message="O nome do produto deve conter no mínimo 3 e máximo 255 caracteres.")
+	private String nome;
+	
+	@NotBlank(message="A descrição do produto é obrigatória.")
+	@Size(min=3,max=1000,message="A descrição do produto deve conter no mínimo 3 e máximo 1000 caracteres.")
+	private String descricao;
+	
 }
